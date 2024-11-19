079ee8940720d1cf84777176411a7e75305feaff
wocks1123 <wocks1123@gmail.com>
2024-11-20 03:02:23 +0900

    remove: 불필요파일 삭제

--- ragtest/src/main/java/com/example/ragtest/adoption/Dog.java
+++
@@ -1,8 +0,0 @@
-package com.example.ragtest.adoption;
-
-import org.springframework.data.annotation.Id;
-
-import java.time.LocalDate;
-
-record Dog(@Id Integer id, String name, String description, LocalDate dob, String owner) {
-}
--- ragtest/src/main/java/com/example/ragtest/adoption/DogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ragtest.adoption;
-
-
-import org.springframework.data.repository.ListCrudRepository;
-
-interface DogRepository extends ListCrudRepository<Dog, Integer> {
-}
